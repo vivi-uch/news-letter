050334e99725faaa5c5579da3d9f332e5a995808
vivi-uch <vivianokonkwouch>
2024-09-07 15:40:46 +0100

    tablet-responsiveness

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -14,7 +14,7 @@ export default function Nav() {
 
   return (
     <div className="flex justify-between items-center">
-      <img src="images/logo.svg" alt="logo" className="w-12" />
+      <img src="images/logo.svg" alt="logo" className="w-12 lg:w-16" />
 
       {/* {mobile screens} */}
       <button onClick={() => setIsOpen(!isOpen)} className="w-12 lg:hidden">
@@ -42,17 +42,22 @@ export default function Nav() {
       {/* {mobile section} */}
       {isOpen && (
         <div className="fixed inset-0 z-50 bg-gray-800 bg-opacity-75">
-          <div className="bg-white p-6 w-3/4 absolute left-1/3 h-full">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="absolute top-4 right-[4.5rem]"
-            >
-              <img
-                src="images/icon-menu-close.svg"
-                alt="close-button"
-                className="w-6"
-              />
-            </button>
+          <div className="bg-white p-6 md:p-8 w-3/4 md:w-1/2 absolute left-1/3 md:left-1/2  h-full">
+            <div className="flex justify-between px-8">
+              <div></div>
+              <div>
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  // className="absolute top-4 right-[4.5rem]"
+                >
+                  <img
+                    src="images/icon-menu-close.svg"
+                    alt="close-button"
+                    className="w-6"
+                  />
+                </button>
+              </div>
+            </div>
 
             <nav className="flex flex-col gap-2 mt-24 text-lg text-gray-800 font-semibold">
               <a href="#home" className="hover:text-gray-600 ">
